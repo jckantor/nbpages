0b10eb40c85abb6b9d3b7db717a28fde6cb4331c
Jeffrey Kantor <kantor.1@nd.edu>
2019-06-28 09:26:32 -0500

    update version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name = "nbpages",
-    version = "0.0.1.dev7",
+    version = "0.0.1.dev8",
     packages=["nbpages", ],
     entry_points = {
         'console_scripts': [
